136ac36fe5606cc0df0fd336e45de0af20a65ee7
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-03-10 22:46:16 -0400

    fixed

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const categoryValidation = require('./validation/categoryValidation');
 const { addProducts } = require('./products/controllers/productControllers');
-
+const {getAddCategory,createCategory} = require('./categories/controllers/categoryControllers')
 router.get('/add-category', getAddCategory);
 router.post('/add-category', categoryValidation, createCategory);
 router.get('/add-products/:name', addProducts);
